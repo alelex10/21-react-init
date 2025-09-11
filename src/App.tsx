e7670ca7195fd894f906351cdd1f728cfe564f16
alelex10 <elvizvida@gmail.com>
2025-09-10 23:33:01 -0300

    feat: component task finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,9 @@
-import { useState } from "react";
-import "./App.css";
-import { CreateTask } from "./components/task/CreateTask";
-import { CardTask, type PropsTask } from "./components/task/CardTask";
+import { TaskPage } from "./components/task/TaskPage";
 
 export type filterType = "all" | "completed" | "pending";
 
 function App() {
-	const [filter, setFilter] = useState<filterType>("pending");
-	const [tasks, setTasks] = useState<PropsTask[]>([
-		{ id: 1, title: "Tarefa 1", description: "Descrição da tarefa 1", completed: false },
-		{ id: 2, title: "Tarefa 2", description: "Descrição da tarefa 2", completed: true },
-	]);
-
-	let filteredTasks: PropsTask[] = [];
-	if (filter === "all") {
-		filteredTasks = tasks;
-	} else if (filter === "completed") {
-		filteredTasks = tasks.filter((task) => task.completed);
-	} else if (filter === "pending") {
-		filteredTasks = tasks.filter((task) => !task.completed);
-	}
-	return (
-		<>
-			<CreateTask setTasks={setTasks} setFilter={setFilter}>
-				{filteredTasks.map((task) => {
-					return (
-						<CardTask
-							key={task.id}
-							id={task.id}
-							title={task.title}
-							description={task.description}
-							completed={task.completed}
-							setTasks={setTasks}
-						/>
-					);
-				})}
-			</CreateTask>
-		</>
-	);
+	return <TaskPage />;
 }
 
 export default App;
-
---
+++ src/components/task/TaskPage.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import type { filterType } from "../../App";
+import { CardTask, type PropsTask } from "./CardTask";
+import { CreateTask } from "./CreateTask";
+
+export const TaskPage = () => {
+	const [filter, setFilter] = useState<filterType>("all");
+	const [tasks, setTasks] = useState<PropsTask[]>([
+		{ id: 1, title: "Tarefa 1", description: "Descrição da tarefa 1", completed: false },
+		{ id: 2, title: "Tarefa 2", description: "Descrição da tarefa 2", completed: true },
+	]);
+
+	let filteredTasks: PropsTask[] = [];
+	if (filter === "all") {
+		filteredTasks = tasks;
+	} else if (filter === "completed") {
+		filteredTasks = tasks.filter((task) => task.completed);
+	} else if (filter === "pending") {
+		filteredTasks = tasks.filter((task) => !task.completed);
+	}
+	return (
+		<>
+			<CreateTask setTasks={setTasks} setFilter={setFilter}>
+				{filteredTasks.map((task) => {
+					return (
+						<CardTask
+							key={task.id}
+							id={task.id}
+							title={task.title}
+							description={task.description}
+							completed={task.completed}
+							setTasks={setTasks}
+						/>
+					);
+				})}
+			</CreateTask>
+		</>
+	);
+};
+
